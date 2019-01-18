ad19aa388c7852b6980016997518d4d971154997
Zhiying Qian <zhiyingq@uci.edu>
2019-01-17 19:36:36 -0800

    get player info from nba

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import nba from 'nba';
+import { Profile } from "./Profile";
 
 export class Main extends React.Component {
+    state = {
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: {}
+    };
+
+    componentDidMount() {
+        nba.stats.playerInfo({ PlayerID: this.state.playerId })
+            .then((info) => {
+                let playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+                console.log('playerInfo', playerInfo);
+                this.setState({ playerInfo });
+            })
+            .catch((e) => console.log(e));
+    }
+
     render() {
         return (
             <div className={"main"}>
-                <div>Profile</div>
+                <Profile playerInfo={this.state.playerInfo}/>
                 <div>ShotChart</div>
             </div>
         );
